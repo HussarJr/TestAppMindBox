70d4961b642dee1548614b2442a13f8d1727387b
Ivan Gusarov <igusarov@live.com>
2021-08-29 16:52:38 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryGeometry/Geometry_lib.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ClassLibraryGeometry
+{
+    public class Geometry_lib
+    {
+        public static double Sfigure(double radius) //calculates the area of a circle by radius
+        {
+            const double PI = 3.141;
+            double s = PI * (radius * radius);
+            return s;
+        }
+        public static double Sfigure(double a, double b, double c) //calculates the area of a triangle on 3 sides
+        {
+            double P = (a + b + c) / 2;
+            double s = Math.Sqrt(P * (P - a) * (P - b) * (P - c));
+            return s;
+        }
+        public static bool Check_triangle(double a, double b, double c)
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                double x;
+                switch (i)
+                {
+                    case 0:
+                        x = ((a * a) + (c * c) - (b * b)) / (2 * a * c);
+                        if (x == 0)
+                        {
+                            count++;
+                        }
+                        break;
+                    case 1:
+                        x = ((a * a) + (b * b) - (c * c)) / (2 * a * b);
+                        if (x == 0)
+                        {
+                            count++;
+                        }
+                        break;
+                    case 2:
+                        x = ((b * b) + (c * c) - (a * a)) / (2 * c * b);
+                        if (x == 0)
+                        {
+                            count++;
+                        }
+                        break;
+                }
+            }
+            if (count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TestAppMindBox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibraryGeometry;
+
+namespace TestAppMindBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Geometry_lib.Sfigure(20));
+            Console.WriteLine(Geometry_lib.Sfigure(213,213,123));
+            Console.WriteLine(Geometry_lib.Check_triangle(213, 213, 123));
+        }
+    }
+}

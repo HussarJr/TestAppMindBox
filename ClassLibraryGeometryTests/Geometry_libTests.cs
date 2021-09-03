8fbc514d09f34d6d697c46f39d07c40988b6e795
Ivan Gusarov <igusarov@live.com>
2021-09-03 15:11:07 +0300

    test

---
+++ ClassLibraryGeometryTests/Geometry_libTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryGeometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryGeometry.Tests
+{
+    [TestClass()]
+    public class Geometry_libTests
+    {
+        [TestMethod()]
+        public void SfigureTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SfigureTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Check_triangleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SfigureTest2()
+        {
+            Assert.Fail();
+        }
+    }
+}

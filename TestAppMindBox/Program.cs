2d70d701912c2df292b94784fd7494938f312294
Ivan Gusarov <63926083+HussarJr@users.noreply.github.com>
2023-02-07 15:57:54 +0300

    Update Program.cs

--- TestAppMindBox/Program.cs
+++ TestAppMindBox/Program.cs
@@ -8,11 +8,9 @@ namespace TestAppMindBox
         static void Main(string[] args)
         {
             Geometry_lib geometry_Lib = new Geometry_lib();
-            string sql = "Select * from products join categories on products.id = categories.products_id ";
             Console.WriteLine(geometry_Lib.Sfigure(20));
             Console.WriteLine(geometry_Lib.Sfigure(213,213,123));
             Console.WriteLine(geometry_Lib.Check_triangle(213, 213, 123));
-            Console.WriteLine(sql);
         }
     }
 }
